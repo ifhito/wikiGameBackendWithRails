6bd76cc606e301a245dbb3ce95b32fe0ef3d5624
ifhito <hito01010101@gmail.com>
2022-09-17 22:34:52 +0900

    test: いくつかテストを書いた

--- spec/channels/wiki_game_channel_channel_spec.rb
+++ spec/channels/wiki_game_channel_channel_spec.rb
@@ -8,15 +8,42 @@ RSpec.describe WikiGameChannel, type: :channel do
   # end
 
   describe "メッセージの送信" do
-    # before do
-    #   @message = build(:message, chat_group_id: @chat_group.id)
-    # end
 
     context "送信成功" do
-      it "グループが選択されている状態でメッセージを送信するとメッセージがひとつDBに保存される" do
+      subject do
+        # `subscribe`ヘルパーは、記載されているチャンネルへ
+        # のサブスクライブアクションを実行する
         subscribe(room: "test", name: "Ayumu")
-        expect(subscription).to be_confirmed
+        # `subscription`はサブスクライブされたチャネルのインスタンス
         subscription
+      end
+      it "部屋にユーザが追加されると、redisに部屋とユーザが追加される" do
+        subscribe(room: "test", name: "Ayumu")
+        expect($redis.lrange("test", 0, -1)).to eq ["Ayumu"]
+        expect($redis.keys('*')).to eq ["test"]
+      end
+
+      it "部屋が作成されるとそのユーザの答えのタイトル、接続の数、部屋のID, 参加者名がブロードキャストされる" do
+        expect{subject}.to have_broadcasted_to("wikigame_channel_test").with{ |data|
+          expect(data['message']['connectNumber']).to eq 0
+          expect(data['message']['answerTitle']).to_not eq ''
+          expect(data['message']['roomId']).to_not eq ''
+        }
+      end
+      # {"data": html, 'nextNumber':0, 'nextName': name_list[0]}
+      it "ゲームがスタートするとシードのHTML、現在の参加者の数、name_listの初めの名前が次のユーザとして追加される" do
+      end
+
+      it "send_urlが呼ばれた場合、渡されたURLのHTMLと次の名前、次の数字が渡される" do
+      end
+
+      it "decied_winnerが決まると勝利者の名前が渡される" do
+      end
+
+      it "delete_userが呼ばれると、渡されたデータのユーザがredisから削除され、次の順番のユーザの番号がフロントに渡される。" do
+      end
+
+      it "delete_userで最後のユーザが削除されるとredisからroomが削除されることを確認する" do
       end
     end
   end
