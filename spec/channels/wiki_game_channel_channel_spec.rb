e1bbb113d1b7a23c9d91521488b4f467387ba5be
ifhito <hito01010101@gmail.com>
2022-09-18 23:34:23 +0900

    test: テストの追加

--- spec/channels/wiki_game_channel_channel_spec.rb
+++ spec/channels/wiki_game_channel_channel_spec.rb
@@ -7,43 +7,61 @@ RSpec.describe WikiGameChannel, type: :channel do
   #   stub_connection 
   # end
 
-  describe "メッセージの送信" do
+  describe 'メッセージの送信' do
 
-    context "送信成功" do
+    context '送信成功' do
       subject do
         # `subscribe`ヘルパーは、記載されているチャンネルへ
         # のサブスクライブアクションを実行する
-        subscribe(room: "test", name: "Ayumu")
+        subscribe(room: 'test', name: 'testuser')
         # `subscription`はサブスクライブされたチャネルのインスタンス
         subscription
       end
-      it "部屋にユーザが追加されると、redisに部屋とユーザが追加される" do
-        subscribe(room: "test", name: "Ayumu")
-        expect($redis.lrange("test", 0, -1)).to eq ["Ayumu"]
-        expect($redis.keys('*')).to eq ["test"]
+      it '部屋にユーザが追加されると、redisに部屋とユーザが追加される' do
+        subscribe(room: 'test', name: 'testuser')
+        expect($redis.lrange('test', 0, -1)).to eq ['testuser']
+        expect($redis.keys('*')).to eq ['test']
       end
 
-      it "部屋が作成されるとそのユーザの答えのタイトル、接続の数、部屋のID, 参加者名がブロードキャストされる" do
-        expect{subject}.to have_broadcasted_to("wikigame_channel_test").with{ |data|
+      it '部屋が作成されるとそのユーザの答えのタイトル、接続の数、部屋のID, 参加者名がブロードキャストされる' do
+        expect{subject}.to have_broadcasted_to('wikigame_channel_test').with{ |data|
           expect(data['message']['connectNumber']).to eq 0
           expect(data['message']['answerTitle']).to_not eq ''
           expect(data['message']['roomId']).to_not eq ''
         }
       end
-      # {"data": html, 'nextNumber':0, 'nextName': name_list[0]}
-      it "ゲームがスタートするとシードのHTML、現在の参加者の数、name_listの初めの名前が次のユーザとして追加される" do
+      # {'data': html, 'nextNumber':0, 'nextName': name_list[0]}
+      it 'ゲームがスタートするとシードのHTML、現在の参加者の数、name_listの初めの名前が次のユーザとして追加される' do
+        subscribe(room: 'test', name: 'testuser')
+        subscribe(room: 'test', name: 'testuser2')
+        expect do perform :start_game, message: {
+          command: 'message',
+          identifier: {
+            channel: 'WikiGameChannel',
+            room: 'test',
+            name: 'testuser'
+        },
+          data: {action: 'start_game'}
+          }
+        end .to have_broadcasted_to('wikigame_channel_test').with{ |data|
+         expect(data['message']['data']).to_not eq ''
+         expect(data['message']['nextNumber']).to eq 0
+         name_list = SubscriberTracker.sub_get_list('test')
+         expect(data['message']['nextName']).to eq name_list[0]
+        }
       end
 
-      it "send_urlが呼ばれた場合、渡されたURLのHTMLと次の名前、次の数字が渡される" do
+      it 'send_urlが呼ばれた場合、渡されたURLのHTMLと次の名前、次の数字が渡される' do
+        
       end
 
-      it "decied_winnerが決まると勝利者の名前が渡される" do
+      it 'decied_winnerが決まると勝利者の名前が渡される' do
       end
 
-      it "delete_userが呼ばれると、渡されたデータのユーザがredisから削除され、次の順番のユーザの番号がフロントに渡される。" do
+      it 'delete_userが呼ばれると、渡されたデータのユーザがredisから削除され、次の順番のユーザの番号がフロントに渡される。' do
       end
 
-      it "delete_userで最後のユーザが削除されるとredisからroomが削除されることを確認する" do
+      it 'delete_userで最後のユーザが削除されるとredisからroomが削除されることを確認する' do
       end
     end
   end
